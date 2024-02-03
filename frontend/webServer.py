77ec38a2fe3ff39d61678309784d3184ba0550d7
Josh Walker <josh.walker1@outlook.com>
2024-02-03 22:32:35 +0000

    Set up .html pages on website

---
+++ frontend/webServer.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+ 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+ 
+@app.route('/search-results-page.html', methods=['GET', 'POST'])
+def search():
+    return render_template('results.html')
+
+@app.route('/login-page.html', methods=['GET', 'POST'])
+def login():
+    return render_template('login-page.html')
+
+
+app.run(host='localhost', port=5000)
